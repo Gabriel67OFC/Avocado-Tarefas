fa0bcb64081a4dd9d91f0cb82e863455d749a7b4
João Gabriel Azevedo Lindoso <joaogabrielgb9899@gmail.com>
2026-09-07 18:35:51 -0300

    tela login avocado 2.0.0

--- src/main/java/com/gabriel/avocado/controller/CadastroController.java
+++ src/main/java/com/gabriel/avocado/controller/CadastroController.java
@@ -1,4 +1,4 @@
 package com.gabriel.avocado.controller;
 
-public class nome {
+public class CadastroController {
 }
--- src/main/java/com/gabriel/avocado/controller/LoginController.java
+++ src/main/java/com/gabriel/avocado/controller/LoginController.java
@@ -12,32 +12,4 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class LoginController {
-    @FXML
-    private TextField senha;
-
-    @FXML
-    private TextField email;
-
-    @FXML
-    private TextFlow senhaIncorreta;
-
-    private String emailCadastrado = "Gabriel@123";
-    private String senhaCadastrada = "1234";
-
-
-    @FXML
-    public void aoLogar(ActionEvent event) throws IOException {
-        if (emailCadastrado.equalsIgnoreCase(email.getText()) && senhaCadastrada.equals(senha.getText())){
-            FXMLLoader fxmlLoader = new FXMLLoader(TelaLogin.class.getResource("home.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 1300, 1000);
-            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            stage.setTitle("Home");
-            stage.setScene(scene);
-            stage.show();
-        }else {
-            senhaIncorreta.setVisible(true);
-        }
-
-    }
-}
+public class LoginController {}
