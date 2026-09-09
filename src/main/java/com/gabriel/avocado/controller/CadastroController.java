5c48499b000641d26c6787f1c4c289019fbb3bb5
= <bielzinhodograuofc@gmail.com>
2026-09-09 15:52:10 -0300

    Comeco dos codigos do Avocado

--- src/main/java/com/gabriel/avocado/controller/CadastroController.java
+++ src/main/java/com/gabriel/avocado/controller/CadastroController.java
@@ -1,4 +1,22 @@
 package com.gabriel.avocado.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class CadastroController {
+
+    @FXML
+    private TextField usuarioNome;
+
+    @FXML
+    private TextField usuarioEmail;
+
+    @FXML
+    private TextField senhaCadastrar;
+
+    @FXML
+    private TextField confirmaSenha;
+
+
 }
